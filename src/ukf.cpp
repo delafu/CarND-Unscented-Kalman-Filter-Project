2973d120160531ab11fa75e7a47d5ff2ace3a5d8
Jose Antonio de la Fuente Sanz <LDAJFS@lda>
2017-10-31 23:26:17 +0100

    new version

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -119,7 +119,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
       x_(1) = meas_package.raw_measurements_(1);
 
     } else if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
-      float ro = meas_package.raw_measurements_(0);
+      float rho = meas_package.raw_measurements_(0);
       float phi = meas_package.raw_measurements_(1);
       float ro_dot = meas_package.raw_measurements_(2);
       x_ << rho * cos(phi), rho * sin(phi), rho_dot * cos(phi), rho_dot * sin(phi);
